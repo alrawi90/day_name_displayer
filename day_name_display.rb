f1cb14490f1fa0247cee3cc56a123081c2fbd8b0
Ali Alrawi <ali.developer90@gmail.com>
2016-12-13 11:48:45 +0300

    Done

---
+++ day_name_display.rb
@@ -0,0 +1,47 @@
+class Day_name_displayer
+   
+   def initialize
+
+     menu
+   end
+   def welcome
+     puts "welcome to ---Day Name Displayer----"
+     puts ""
+   end
+   def msg
+    puts	"Please enter a number from 1-7 OR enter 'exit' to end application ."
+   end
+   def menu
+    	msg
+    	input=nil
+    	while(input !="exit")
+    		puts ""
+    		input=gets.strip
+            if input =="1"
+                puts "Sunday"
+            elsif input =="2"
+                puts "Monday"
+            elsif input=="3"
+             	  puts "Tuesday"
+            elsif input=="4"
+             	  puts "Wednesday"
+            elsif input=="5"
+             	  puts "Thursday"
+            elsif input=="6"
+             	  puts "Friday"
+            elsif input=="7"
+             	  puts "Saturday"             	              	               	  
+            elsif input=="exit"
+                puts "Thank you for using  --Day Name Displayer--" 
+                puts ""
+                break
+            else
+              puts "invalid input"
+              msg
+            end
+	    	end
+	    end
+ 
+
+end
+obj=Day_name_displayer.new
